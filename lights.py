fcbc4986801fa77bbe0a5b76b59a36937cbb8a64
Chris Wang <chriswang444@gmail.com>
2017-11-23 14:21:40 -0600

    Update import setup

--- lights.py
+++ lights.py
@@ -1,15 +1,23 @@
-rpi = True
+from configparser import ConfigParser
+from pygame import mixer
+from time import sleep
+
+pins = (19, 13, 6, 5, 11, 9, 10, 22, 2, 3, 4, 17, 27)
 
 try:
     import colorama
 
     colorama.init()
 except ImportError:
-    pass
+    colorama = None
+    print("Get colorama if you're on Windows pls.")
 # gpiozero controls the pins.
 try:
-    from gpiozero import LED
+    from gpiozero import LEDBoard
+
+    pins = LEDBoard(*pins)
 except ImportError:
-    rpi = False
-import pygame
-from time import sleep
+    LEDBoard = None  # Get rid of PyCharm warning.
+    pins = None
+
+songs = {}
